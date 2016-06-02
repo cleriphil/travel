06e2cb4735759f53057b5701ce336a6e920f0115
cleriphil <cleri.phil@gmail.com>
2016-06-01 19:57:56 -0700

    add styling and display each photo

--- app/js/controllers/PhotosController.js
+++ app/js/controllers/PhotosController.js
@@ -1,21 +1,21 @@
 'use strict';
 
-photoApp.controller('photosController',
-  function photosController($scope) {
+photoApp.controller('PhotosController',
+  function PhotosController($scope) {
     $scope.photos = [
       {
         country: "Cambodia",
-        imageUrl: "",
+        imageUrl: "https://static.pexels.com/photos/9283/landscape-nature-sky-blue-large.jpg",
         likeCount: 0
       },
       {
         country: "France",
-        imageUrl: "",
+        imageUrl: "https://static.pexels.com/photos/92658/pexels-photo-92658-large.jpeg",
         likeCount: 0
       },
       {
         country: "South Africa",
-        imageUrl: "",
+        imageUrl: "https://static.pexels.com/photos/5301/landscape-nature-sky-clouds-large.jpg",
         likeCount: 0
       }
     ]
