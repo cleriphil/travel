a98db5bb559e813127342136d88fd4872e5edb34
cleriphil <cleri.phil@gmail.com>
2016-06-02 19:09:28 -0700

    add order option

--- app/js/controllers/PhotosController.js
+++ app/js/controllers/PhotosController.js
@@ -2,24 +2,31 @@
 
 photoApp.controller('PhotosController',
   function PhotosController($scope) {
+    $scope.sortorder = 'country';
     $scope.photos = [
       {
-        country: "Cambodia",
+        country: "France",
+        city: "Paris",
         imageUrl: "https://static.pexels.com/photos/9283/landscape-nature-sky-blue-large.jpg",
         likeCount: 0,
-        hashTags: []
+        hashTags: [],
+        dateAdded: ""
       },
       {
-        country: "France",
+        country: "Cyprus",
+        city: "Larnaca",
         imageUrl: "https://static.pexels.com/photos/92658/pexels-photo-92658-large.jpeg",
         likeCount: 0,
-        hashTags: []
+        hashTags: [],
+        dateAdded: ""
       },
       {
         country: "South Africa",
+        city: "Cape Town",
         imageUrl: "https://static.pexels.com/photos/5301/landscape-nature-sky-clouds-large.jpg",
         likeCount: 0,
-        hashTags: []
+        hashTags: [],
+        dateAdded: ""
       }
     ]
 
