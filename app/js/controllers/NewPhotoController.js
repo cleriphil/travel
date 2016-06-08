de896e426d8b7eb38f5c79834f1b9c7850501462
cleriphil <cleri.phil@gmail.com>
2016-06-07 23:45:59 -0700

    add ability to enter form to 'save' and cancel

--- app/js/controllers/NewPhotoController.js
+++ app/js/controllers/NewPhotoController.js
@@ -2,4 +2,14 @@
 
 photoApp.controller('NewPhotoController',
   function NewPhotoController($scope) {
-};
+      $scope.savePhoto = function(photo, newPhotoForm) {
+        console.log(newPhotoForm);
+        if(newPhotoForm.$valid) {
+          window.alert('photo: ' + photo.city + ' saved!');
+        }
+      }
+      $scope.cancelNewPhoto = function() {
+        window.location = "/PhotosList.html";
+      }
+  }
+);
