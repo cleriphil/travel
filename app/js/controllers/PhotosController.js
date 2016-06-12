1b0183da99090cbdf503dc7fcd9733db2f14f9a1
cleriphil <cleri.phil@gmail.com>
2016-06-11 21:44:12 -0700

    disable like button aafter clicked

--- app/js/controllers/PhotosController.js
+++ app/js/controllers/PhotosController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 photoApp.controller('PhotosController',
-  function PhotosController($scope, photoData, $log) {
+  function PhotosController($scope, photoData, $log, $filter) {
     $scope.sortorder = 'country';
 
     photoData.getPhoto()
@@ -13,8 +13,21 @@ photoApp.controller('PhotosController',
         $log.warn(data, status, headers(), config);
       });
 
-    $scope.like = function(photo) {
+    $scope.like = function(id) {
+      var idFilter = $filter('filter');
+      var photo = idFilter($scope.photos, {id: id})[0];
       photo.likeCount++;
+      angular.element(document.getElementById('photo' + photo.id))[0].disabled = true;
+    }
+
+    $scope.insertRecord = function(photo) {
+      if($scope.clicked){
+
+        return;
+      }
+      $scope.clicked = true;
     }
   }
 );
+
+//like can only be done once by a user
